2401eb549c45b27581fc8d52aee64379c025f02a
Shivendra Pratap Singh <shivendra680@gmail.com>
2021-06-07 21:23:38 +0530

    2nd

---
+++ Sales by Match.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        int pairs=0;
+        int[] ar1=new int[n];
+        for (int i =0; i <n; i++)
+            ar1[i] = ar.get(i);
+    for(int i=0;i<n;i++){
+        int a=ar1[i];
+        for(int j=i+1;j<n;j++){
+            int b=ar1[j];
+            if(a==b){
+                if(b!=-1){
+                pairs++;
+                ar1[i]=-1;
+                ar1[j]=-1;
+                break;
+            }}
+        }
+    }return pairs;}
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
